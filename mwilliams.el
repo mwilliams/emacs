2cf338cbf783f3db4fcffbee7b2c75ff0be3d02d
Matthew Williams <matthew.d.williams@gmail.com>
2010-01-13 11:47:46 -1000

    Removing Textmate.el as it's in ELPA right now.

--- mwilliams.el
+++ mwilliams.el
@@ -44,11 +44,6 @@
 (setq default-tab-width 2)
 (setq tab-width 2)
 
-;; Textmate.el from Defunkt
-(add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
-(require 'textmate)
-(textmate-mode)
-
 ;; Color theme
 (add-to-list 'load-path "~/.emacs.d/vendor/color-theme")
 (require 'color-theme)
