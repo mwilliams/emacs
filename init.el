1df3f48722a878329dbc1efd41eb446c655cfe66
Matthew Williams <matthew.d.williams@gmail.com>
2010-01-12 16:33:55 -1000

    Revert "Removing ELPA stuff from init.el"
    This reverts commit e263edb5d8c8568e1fec85d47e6c85c6cacc35d0.

--- init.el
+++ init.el
@@ -30,3 +30,14 @@
 ;;; interfacing with ELPA, the package archive.
 ;;; Move this code earlier if you want to reference
 ;;; packages in your .emacs.
+
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
